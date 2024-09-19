f538dcba091dfe44a863fbc03e31e949b0a2b6b4
igorferreira10 <143271211+igorferreira10@users.noreply.github.com>
2024-09-19 14:57:07 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Projeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
